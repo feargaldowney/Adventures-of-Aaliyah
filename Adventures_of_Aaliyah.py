dd1afcf184749c8406afde1e29c0357368eb4628
Feargal Downey <feargal.downey@mycit.ie>
2022-09-24 00:03:09 +0100

    Wrote a better introduction

--- Adventures_of_Aaliyah.py
+++ Adventures_of_Aaliyah.py
@@ -20,20 +20,25 @@ def faketype(words):
 
 
 
+greg_rp = 0
+
 def start():
   # give some prompts.
-  print("\nYou are standing in a dark room.")
-  print("There is a door to your left and right, which one do you take? (l or r)")
-  
-  # convert the player's input() to lower_case
-  answer = input(">").lower()
+  faketype("\n.....\n...\nA young girl grimaces as her eyes struggle to open in the harsh sunlight.")
+  faketype("\nShe clutches a nearby railing to pull herself up.")
+  faketype("\nThe girl stood up, and within a moment her heart drops...\n\nShuddering as she glances around.")
+  faketype("\nThe smell worse now, then the sight of it.")
+  faketype("\nWill the girl get sick or will she brave the conditions? ")
+  answer = input("\ns - to get sick\nb - to brave conditions  > ").lower()  # convert the player's input() to lower_case
 
-  if "l" in answer:
-    # if player typed "left" or "l" lead him to bear_room()
-    bear_room()
-  elif "r" in answer:
+  
+
+  if "b" in answer:
+    # if player typed "s" gain 1 rp from Greg
+    greg_rp_up()
+  elif "s" in answer:
     # else if player typed "right" or "r" lead him to monster_room()
-    monster_room()
+    greg_rp_down()
   else:
     # else call game_over() function with the "reason" argument
     game_over("Don't you know how to type something properly?")
@@ -41,16 +46,11 @@ def start():
 
 
 # bear room
-def bear_room():
+def greg_rp_up():
   # give some prompts
-  # '\n' is to print the line in a new line
-  print("\nThere is a bear here.")
-  print("Behind the bear is another door.")
-  print("The bear is eating tasty honey!")
-  print("What would you do? (1 or 2)")
-  print("1). Take the honey.")
-  print("2). Lure the bear away.")
+  faketype("\n\"Huh... I'm Impressed...\"\n\"Would have expected a girl your age to be sick at this sight.\"")
 
+  
   # take input()
   answer = input(">")
   
@@ -59,7 +59,7 @@ def bear_room():
     game_over("The bear killed you.")
   elif answer == "2":
     # lead him to the diamond_room()
-    print("\nWell done! You managed to lure the bear away from the door.")
+    faketype("\nWell done! You managed to lure the bear away from the door.")
     diamond_room()
   else:
     # else call game_over() function with the "reason" argument
@@ -68,11 +68,10 @@ def bear_room():
 # monster room
 def monster_room():
   # some prompts
-  # '\n' is to print the line in a new line
-  print("\nNow you entered the room of a monster!")
-  print("The monster is sleeping.\nBehind the monster, there is another door. What would you do? (1 or 2)")
-  print("1). Go through the door silently.")
-  print("2). Kill the monster and show your courage!")
+  faketype("\nNow you entered the room of a monster!")
+  faketype("The monster is sleeping.\nBehind the monster, there is another door. What would you do? (1 or 2)")
+  faketype("1). Go through the door silently.")
+  faketype("2). Kill the monster and show your courage!")
 
   # take input()
   answer = input(">")
@@ -91,11 +90,11 @@ def monster_room():
 # diamond room
 def diamond_room():
   # some prompts
-  print("\nYou are now in a room filled with diamonds!")
-  print("And there is a door too!")
-  print("What would you do? (1 or 2)")
-  print("1). Take some diamonds and go through the door.")
-  print("2). Just go through the door.")
+  faketype("\nYou are now in a room filled with diamonds!")
+  faketype("And there is a door too!")
+  faketype("What would you do? (1 or 2)")
+  faketype("1). Take some diamonds and go through the door.")
+  faketype("2). Just go through the door.")
 
   # take input()
   answer = input(">")
@@ -105,7 +104,7 @@ def diamond_room():
     game_over("They were cursed diamonds! The moment you touched it, the building collapsed, and you die!")
   elif answer == "2":
     # the player won the game
-    print("\nNice, you're are an honest man! Congrats you win the game!")
+    faketype("\nNice, you're are an honest man! Congrats you win the game!")
     # activate play_again() function
     play_again()
   else:
@@ -114,7 +113,7 @@ def diamond_room():
 
 # function to ask play again or not
 def play_again():
-  print("\nDo you want to play again? (y or n)")
+  faketype("\nDo you want to play again? (y or n)")
   
   # convert the player's input to lower_case
   answer = input(">").lower()
@@ -130,8 +129,8 @@ def play_again():
     # game_over function accepts an argument called "reason"
 def game_over(reason):
   # print the "reason" in a new line (\n)
-  print("\n" + reason)
-  print("Game Over!")
+  faketype("\n" + reason)
+  faketype("Game Over!")
   # ask player to play again or not by activating play_again() function
   play_again()
 
